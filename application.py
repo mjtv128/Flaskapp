9c9bb6b0263304eac604ddf51bbe74d45345d828
EC2 Default User <ec2-user@ip-172-31-17-177.ec2.internal>
2021-09-03 03:35:59 +0000

    setup

---
+++ application.py
@@ -0,0 +1,31 @@
+from flask import Flask
+
+# print a nice greeting.
+def say_hello(username = "World"):
+    return '<p>Hello %s!</p>\n' % username
+
+# some bits of text for the page.
+header_text = '''
+    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
+instructions = '''
+    <p><em>Hint</em>: This is a RESTful web service! Append a username
+    to the URL (for example: <code>/Thelonious</code>) to say hello to
+    someone specific.</p>\n'''
+home_link = '<p><a href="/">Back</a></p>\n'
+footer_text = '</body>\n</html>'
+
+# EB looks for an 'application' callable by default.
+application = Flask(__name__)
+
+@application.route("/")
+def hello():
+    """Return a friendly HTTP greeting."""
+    print("I am inside hello world")
+    return "Continuous Delivery Demo"
+
+# run the app.
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = True
+    application.run(host='0.0.0.0')
